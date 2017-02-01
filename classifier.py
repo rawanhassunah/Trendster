1c7f8f08e3e38508e1ee864f379d02677e1da66d
Rawan Hassunah <rawanhassunah@gmail.com>
2017-01-31 17:38:59 -0800

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -2,7 +2,7 @@ from __future__ import division
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split
 import nltk
 from nltk import word_tokenize
